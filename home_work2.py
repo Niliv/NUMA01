48ee5287bfb3d3550db516d9b82bc01d77af36e2
Niliv <112508099+Niliv@users.noreply.github.com>
2024-07-11 15:29:16 +0200

    Task4

--- home_work2.py
+++ home_work2.py
@@ -2,39 +2,46 @@ class Interval():
     def __init__(self,a ,b):
         self.lb = a
         self.ub = b
-    
-
-    def value(self):
-        return(self.lb, self.ub)
 
 
-    def add(self, x):
-        self.lb += x.lb
-        self.ub += x.ub
+    def __str__(self):
+        return "[" + str(self.lb) + "," + str(self.ub) + "]"
 
 
-    def sub(self, x):
-        self.lb -= x.ub
-        self.ub -= x.lb
+    def __add__(self, x):
+        new_lb = self.lb + x.lb
+        new_ub = self.ub + x.ub
+        return Interval(new_lb, new_ub)
 
 
-    def mul(self, x):    
-        self.lb = min(self.lb * x.lb, self.lb * x.ub, self.ub * x.lb, self.ub * x.ub)
-        self.ub = max(self.lb * x.lb, self.lb * x.ub, self.ub * x.lb, self.ub * x.ub)
+    def __sub__(self, x):
+        new_lb = self.lb - x.ub
+        new_ub = self.ub - x.lb
+        return Interval(new_lb, new_ub)
+        
+
+    def __mul__(self, x):    
+        new_lb = min(self.lb * x.lb, self.lb * x.ub, self.ub * x.lb, self.ub * x.ub)
+        new_ub = max(self.lb * x.lb, self.lb * x.ub, self.ub * x.lb, self.ub * x.ub)
+        return Interval(new_lb, new_ub)
 
 
-    def div(self, x):
-        self.lb = min(self.lb / x.lb, self.lb / x.ub, self.ub / x.lb, self.ub / x.ub)
-        self.ub = max(self.lb / x.lb, self.lb / x.ub, self.ub / x.lb, self.ub / x.ub)
+    def __truediv__(self, x):
+        new_lb = min(self.lb / x.lb, self.lb / x.ub, self.ub / x.lb, self.ub / x.ub)
+        new_ub = max(self.lb / x.lb, self.lb / x.ub, self.ub / x.lb, self.ub / x.ub)
+        return Interval(new_lb, new_ub)
     
 
 def main():
-    in1 = Interval(5,6)
-    in2 = Interval(3,4)
-    in1.div(in2)
+    in1 = Interval(1,4)
+    in2 = Interval(-2,-1)
+    
 
 
-    print(in1.value())
+    print(in1 + in2)
+    print(in1 - in2)
+    print(in1 * in2)
+    print(in1 / in2)
 
 
 
