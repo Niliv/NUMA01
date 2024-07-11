3629d284350254d324e1f36efc4feb2f2d219587
Niliv <112508099+Niliv@users.noreply.github.com>
2024-07-11 15:45:21 +0200

    Task7

--- home_work2.py
+++ home_work2.py
@@ -1,43 +1,54 @@
 class Interval():
-    def __init__(self,a ,b):
-        self.lb = a
-        self.ub = b
+    def __init__(self, *args):
+
+        if len(args) > 1:
+            self.lb = args[0]
+            self.ub = args[1]
+        else:
+            self.lb = args[0]
+            self.ub = args[0]
+    
+
+    
 
 
     def __str__(self):
         return "[" + str(self.lb) + "," + str(self.ub) + "]"
+    
+    
+    def __contains__(self, r):
+        return self.lb <= r < self.ub
 
 
     def __add__(self, x):
-        new_lb = self.lb + x.lb
-        new_ub = self.ub + x.ub
-        return Interval(new_lb, new_ub)
+        return Interval(self.lb + x.lb, self.ub + x.ub)
 
 
     def __sub__(self, x):
-        new_lb = self.lb - x.ub
-        new_ub = self.ub - x.lb
-        return Interval(new_lb, new_ub)
+        return Interval(self.lb - x.ub, self.ub - x.lb)
         
 
     def __mul__(self, x):    
-        new_lb = min(self.lb * x.lb, self.lb * x.ub, self.ub * x.lb, self.ub * x.ub)
-        new_ub = max(self.lb * x.lb, self.lb * x.ub, self.ub * x.lb, self.ub * x.ub)
-        return Interval(new_lb, new_ub)
+        products = [self.lb * x.lb, self.lb * x.ub, self.ub * x.lb, self.ub * x.ub]
+        return Interval(min(products), max(products))
 
 
     def __truediv__(self, x):
-        new_lb = min(self.lb / x.lb, self.lb / x.ub, self.ub / x.lb, self.ub / x.ub)
-        new_ub = max(self.lb / x.lb, self.lb / x.ub, self.ub / x.lb, self.ub / x.ub)
-        return Interval(new_lb, new_ub)
+
+        if x.__contains__(0):
+            raise ZeroDivisionError("Interval division by zero is undefined")
+        
+        divsions = [self.lb / x.lb, self.lb / x.ub, self.ub / x.lb, self.ub / x.ub]
+        return Interval(min(divsions), max(divsions))
     
 
 def main():
-    in1 = Interval(1,4)
-    in2 = Interval(-2,-1)
+    in1 = Interval(1)
+    in2 = Interval(-2,1)
     
 
 
+    exit()
     print(in1 + in2)
     print(in1 - in2)
     print(in1 * in2)
