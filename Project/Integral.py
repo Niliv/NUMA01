97e00e491dab3610265224a27edc42699082afce
Niliv <nilsaxelivarsson@gmail.com>
2024-08-19 22:16:49 +0200

    Final code

--- Project/Integral.py
+++ Project/Integral.py
@@ -1,6 +1,7 @@
 import numpy as np
 import Mesh as mesh
 import Point as point
+import matplotlib.pyplot as plt
 
 def readCords(filepath):
     
@@ -27,66 +28,25 @@ def readElem(filepath):
     return elems
 
 
-# Concept and proof of formula:
-# https://www.cuemath.com/geometry/area-of-triangle-in-coordinate-geometry/
-def calcArea(mesh):
-    totalArea = 0
-    cords, elements = mesh.mesh
-
-    for n, elem in enumerate(elements):
-        e1, e2, e3 = elem
-
-        p1 = point.Point(cords[0][e1 -1], cords[1][e1 -1])
-        p2 = point.Point(cords[0][e2 -1], cords[1][e2 -1])
-        p3 = point.Point(cords[0][e3 -1], cords[1][e3 -1])
-
-        area = 0.5 * abs(p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y))
-        #area = 0.5 * abs(mesh.determinants[n])
-
-        totalArea += area
-
-
-    return totalArea
-
-def calcIntegral(mesh, fun):
-    partialIntegrals = []
-    cords, elements = mesh.mesh
-    
-    for n, elem in enumerate(elements):
-        e1, e2, e3 = elem
-
-        p1 = point.Point(cords[0][e1 -1], cords[1][e1 -1])
-        p2 = point.Point(cords[0][e2 -1], cords[1][e2 -1])
-        p3 = point.Point(cords[0][e3 -1], cords[1][e3 -1])
-        #print(p1,p2,p3)
-        #print(fun(p1)+fun(p2)+fun(p3))
-        I_unit = 1/6 * (fun(p1)+fun(p2)+fun(p3)) * abs(mesh.determinants[n])
-
-        partialIntegrals.append(I_unit)
-    
-
-    return sum(partialIntegrals)
-
-
-
-    
-
-
-    
-
-
 def function(p):
     return 1
 
 if __name__ == "__main__":
-    cords = readCords("./meshes/coord2.txt")
-    elem = readElem("./meshes/elementnode2.txt")
+    cords = readCords("./meshes/coordinates_dolfin_coarse.txt")
+    elem = readElem("./meshes/nodes_dolfin_coarse.txt")
     
     mesh = mesh.Mesh(cords, elem)
 
-    #print(calcIntegral(mesh, function))
-    #print(calcArea(mesh))
+    area = mesh.area()
 
+    integral = mesh.integral(function)
+
+    print("Total area is ", area)
+    print("The integral of the function over the area is ", integral)
+    print("Difference ", area-integral)
+
+   
     mesh.draw()
+    
 
     
--- Project/Mesh.py
+++ Project/Mesh.py
@@ -2,64 +2,100 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import Point as point
+import random
 
 class Mesh():
 
     def __init__(self, *args):
         self.mesh = (args[0], args[1])
-        self.determinants = self._CalcDeterminants()
-
+        self.determinants = self.__CalcDeterminants__()
 
     def draw(self):
         cords, elements = self.mesh
+        plt.figure()
 
-        radi = 0.05
         for elem in elements:
             e1, e2, e3 = elem
 
+            p1 = point.Point(cords[0][e1 -1], cords[1][e1 -1])
+            p2 = point.Point(cords[0][e2 -1], cords[1][e2 -1])
+            p3 = point.Point(cords[0][e3 -1], cords[1][e3 -1])
+            
+            plt.fill([p1.x, p2.x, p3.x, p1.x], [p1.y, p2.y, p3.y, p1.y])
+            plt.plot([p1.x, p2.x, p3.x, p1.x], [p1.y, p2.y, p3.y, p1.y], color="white")
+            
+        plt.show()    
+
+
+    # Concept and proof of formula:
+    # https://www.cuemath.com/geometry/area-of-triangle-in-coordinate-geometry/
+    def area(self):
+        totalArea = 0
+        cords, elements = self.mesh
+
+        for elem in elements:
+            e1, e2, e3 = elem
 
             p1 = point.Point(cords[0][e1 -1], cords[1][e1 -1])
             p2 = point.Point(cords[0][e2 -1], cords[1][e2 -1])
             p3 = point.Point(cords[0][e3 -1], cords[1][e3 -1])
 
-            
-            
-            
-        
+            area = 0.5 * abs(p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y))
+            #area = 0.5 * abs(mesh.determinants[n])
 
-        plt.show()
+            totalArea += area
+
+        return totalArea
+    
+
+    def integral(self, fun):
+        partialIntegrals = []
+        cords, elements = self.mesh
+
+        for n, elem in enumerate(elements):
+            e1, e2, e3 = elem
+
+            p1 = point.Point(cords[0][e1 -1], cords[1][e1 -1])
+            p2 = point.Point(cords[0][e2 -1], cords[1][e2 -1])
+            p3 = point.Point(cords[0][e3 -1], cords[1][e3 -1])
+
+            I_unit = 1/6 * (fun(p1)+fun(p2)+fun(p3)) * abs(self.determinants[n])
+
+            partialIntegrals.append(I_unit)
 
 
-    def _CalcDeterminants(self):
+        return sum(partialIntegrals)
+
+    def __CalcDeterminants__(self):
         determinants = []
         for elem in self.mesh[1]:
-            if self._minAngle(elem) <= 0:
+            if self.__minAngle__(elem) <= 0:
                 raise ValueError("To small of an angle")
-            determinants.append(self._determinant(elem))
+            determinants.append(self.__determinant__(elem))
         
         return determinants
             
 
-    def _minAngle(self, element):
+    def __minAngle__(self, element):
         cords, elem = self.mesh
         
         e1, e2, e3 = element
 
-        p1 = (cords[0][e1 -1], cords[1][e1 -1])
-        p2 = (cords[0][e2 -1], cords[1][e2 -1])
-        p3 = (cords[0][e3 -1], cords[1][e3 -1])
+        p1 = point.Point(cords[0][e1 -1], cords[1][e1 -1])
+        p2 = point.Point(cords[0][e2 -1], cords[1][e2 -1])
+        p3 = point.Point(cords[0][e3 -1], cords[1][e3 -1])
 
-        a = self._sideLength(p1, p2)
-        b = self._sideLength(p2, p3)
-        c = self._sideLength(p3, p1)
+        a = self.__sideLength__(p1, p2)
+        b = self.__sideLength__(p2, p3)
+        c = self.__sideLength__(p3, p1)
 
-        A = self._calcAngle(a, b, c)
-        B = self._calcAngle(b, a, c)
-        C = self._calcAngle(c, a, b)
+        A = self.__calcAngle__(a, b, c)
+        B = self.__calcAngle__(b, a, c)
+        C = self.__calcAngle__(c, a, b)
    
         return min(A,B,C)
 
-    def _jacobian(self, element):
+    def __jacobian__(self, element):
         cords, elem = self.mesh
         
         e1, e2, e3 = element
@@ -74,20 +110,18 @@ class Mesh():
 
         
 
-    def _determinant(self, element):
+    def __determinant__(self, element):
 
-        J = self._jacobian(element)
+        J = self.__jacobian__(element)
 
         return J[0][0] * J[1][1] - J[0][1] * J[1][0]
     
 
-  
 
-
-    def _calcAngle(self, a, b, c):
+    def __calcAngle__(self, a, b, c):
         return math.degrees(math.acos((b**2 + c**2 - a**2) / (2 * b * c)))
     
 
-    def _sideLength(self, p1, p2):
-        return math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
+    def __sideLength__(self, p1, p2):
+        return math.sqrt((p2.x - p1.x)**2 + (p2.y - p1.y)**2)
 
